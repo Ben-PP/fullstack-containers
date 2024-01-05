e145416b971d203d69d782bc964bb74cb63df0b8
Karel Parkkola <karel.parkkola@gmail.com>
2024-01-05 13:33:27 +0200

    12.12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const redis = require("../redis");
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
+  await redis.setAsync("visits", visits);
 
   res.send({
     ...configs,
-    visits
+    visits: await redis.getAsync("visits"),
+  });
+});
+
+router.get("/statistics", async (req, res) => {
+  res.send({
+    added_todos: parseInt((await redis.getAsync("added_todos")) ?? 0),
   });
 });
 
